fa892bbfe839dcb74e3f9646d76c886cd1d1d0f0
tsumitakaoru <k.tsumita01@gmail.com>
2022-08-30 12:37:47 -0400

    Add lodash

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //jshint esversion:6
 import express from 'express';
 import bodyParser from "body-parser";
+import _ from 'lodash';
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -41,6 +42,18 @@ app.post("/compose", (req, res) => {
   }
   posts = [...posts, post];
   res.redirect("/");
+})
+
+app.get("/posts/:brogTitle", (req, res) => {
+  const lowercaseBrogTitle = _.toLower(req.params.brogTitle);
+  console.log(posts);
+  console.log(lowercaseBrogTitle);
+  for (let i = 0; i < posts.length; i++) {
+    console.log(_.toLower(posts[i].title));
+    if (_.toLower(posts[i].title) === lowercaseBrogTitle) {
+      console.log("herehere");
+    }
+  }
 })
 
 
