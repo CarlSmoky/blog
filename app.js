87469f99b4380013e917166e7eb9626456d6aae3
tsumitakaoru <k.tsumita01@gmail.com>
2022-08-30 10:07:47 -0400

    Add get router to home

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.render("home");
+})
 
 
 
